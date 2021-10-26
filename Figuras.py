e7f6d165a064ad8c8b88083b2ec779530ba29bee
CesarAlejandroSalazar <a01197841@itesm.mx>
2021-10-26 12:28:52 -0500

    Update Figuras.py

--- Figuras.py
+++ Figuras.py
@@ -39,19 +39,22 @@ def circle(start, end):
     end_fill()
 
 def rectangle(start, end):
-    "Draw rectangle from start to end"
+    "Draw rectangle from start to end."
     up()
     goto(start.x, start.y)
     down()
     begin_fill()
 
-    for count in range(2):
-        forward(end.x - start.x)
-        left(90)
-        forward((end.x - start.x)/2)
-        left(90)
+    for count in range(4):
+        if(count%2):
+            forward(end.y - start.y)
+            left(90)
+        else:
+            forward(end.x - start.x)
+            left(90)
 
     end_fill()
+
 
 def triangle(start, end):
     "Draw triangle from start to end."
@@ -62,7 +65,7 @@ def triangle(start, end):
 
     for count in range(3):
         forward(end.x - start.x)
-        left(120)
+        left(-120)
 
     end_fill()
 
