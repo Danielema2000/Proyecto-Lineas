d75d4cd70c2c2a39ec459864e6ce8d7f385c9287
Danielema2000 <a00227518@itesm.mx>
2021-10-26 11:51:32 -0500

    Update Rectangle
    Se implementa la funcion para dibujar rectangulo

--- Figuras.py
+++ Figuras.py
@@ -27,7 +27,22 @@ def circle(start, end):
 
 def rectangle(start, end):
     "Draw rectangle from start to end."
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(4):
+        if(count%2):
+            forward(end.y - start.y)
+            left(90)
+        else:
+            forward(end.x - start.x)
+            left(90)
+
+    end_fill()
+
+
 
 def triangle(start, end):
     "Draw triangle from start to end."
